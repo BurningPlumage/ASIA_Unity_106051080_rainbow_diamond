e1cf428226a8a89dd1bec98fc159d7a8ecb49f10
BurningPlumage <55180708+BurningPlumage@users.noreply.github.com>
2020-01-10 20:19:35 +0800

    v0.4

---
+++ kid3D/Assets/real_assets/script/sttext.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sttext : MonoBehaviour
+{
+    [TextArea(3, 5)]
+    public string s;
+
+    public Text t;
+
+    public bool b;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "play")
+        {
+            t.text = s;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.tag == "play"&&b)
+        {
+            t.text = "";
+        }
+    }
+}
